3e148eda6a62dd5813c6436b1849f9265f973403
Mux <dnfl7080@daum.net>
2018-11-20 14:15:25 +0900

    lol!

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,3 +1,4 @@
+var sw = -1;
 $(document).ready(function () {
 
     var size = $('#wrap').innerWidth();
@@ -6,4 +7,25 @@ $(document).ready(function () {
     $('img').ready(function () {
        $('img').css('width', getSize);
     });
+
+    $('#hide').click(function () {
+            $('img').fadeToggle(700);
+            if (sw == -1) {
+                setTimeout(function () {
+                    $('#hide').css('background-color', '#FFFFFF');
+                    $('#hide').animate({top: '13%'}, 1000);
+                    sw *= -1;
+                }, 700);
+            } else {
+                setTimeout(function () {
+                    $('#hide').css('background-color', '#858281');
+                    $('#hide').animate({top: '30%'}, 1000);
+                    sw *= -1;
+                }, 10);
+            }
+    });
+
+    $('img').click(function() {
+
+    })
 });
