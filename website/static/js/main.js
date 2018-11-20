fc18bf90a86a01e4be52594135fe38b883f024cd
Mux <dnfl7080@daum.net>
2018-11-20 11:08:02 +0900

    well...

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,2 +1,9 @@
 $(document).ready(function () {
+
+    var size = $('#wrap').innerWidth();
+    var getSize = size / 3;
+
+    $('img').ready(function () {
+       $('img').css('width', getSize);
+    });
 });
